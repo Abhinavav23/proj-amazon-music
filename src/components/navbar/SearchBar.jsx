c4017f1febcda4d15f61668232fae9a24af61328
Abhinav <abhinav@AVs-Mac.local>
2023-09-10 12:24:49 +0530

    [10 sep 2023] fix styles

--- src/components/navbar/SearchBar.jsx
+++ src/components/navbar/SearchBar.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { ReactComponent as SearchIcon } from "../../assets/search.svg";
-const style = {
-  display: "flex",
-  alignItems: "center",
-  background: "#fff",
-  padding: '0.5rem',
-  borderRadius: '2.5rem',
-  border: 'none'
-};
+
 export const SearchBar = () => {
   return (
-    <section style={style}>
+    <section className="searchbar-container">
       <input type="text" name="search" id="search" placeholder="search here" />
       <SearchIcon />
     </section>
